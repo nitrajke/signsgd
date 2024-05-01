a4e96f8a83e626c0bf9813266dc9e97b1331c513
Mikhail Ovchinnikov <ovciyl@gmail.com>
2024-05-01 13:39:59 +0400

    Add files via upload

---
+++ signsgd.py
@@ -0,0 +1,35 @@
+import torch
+from torch.optim import Optimizer
+
+
+class SignSGD(Optimizer):
+
+    def __init__(self, params, lr=1e-3, weight_decay=0):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+
+        defaults = dict(lr=lr, weight_decay=weight_decay)
+        super(SignSGD, self).__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad
+                if weight_decay != 0:
+                    d_p = d_p.add(p, alpha=weight_decay)
+
+                p.add_(d_p.sign(), alpha=-group['lr'])
+
+        return loss
